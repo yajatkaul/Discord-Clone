5ed59cc23e6aaf4e3c4bba9e85d601c9df2cebf7
YajatKaul <mc.crafter1973@gmail.com>
2024-05-24 12:01:30 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   prisma/schema.prisma 	deleted:    public/next.svg 	deleted:    public/vercel.svg 	new file:   src/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx 	new file:   src/app/(main)/(routes)/servers/[serverId]/conversations/[memberId]/page.tsx 	modified:   src/app/(main)/(routes)/servers/[serverId]/page.tsx 	new file:   src/app/api/channels/[channelId]/route.ts 	new file:   src/components/chat/chat-header.tsx 	new file:   src/components/mobile-toggle.tsx 	new file:   src/components/modals/delete-channel-modal.tsx 	new file:   src/components/modals/edit-channel-modal.tsx 	modified:   src/components/navigation/navigation-sidebar.tsx 	modified:   src/components/providers/modal-provider.tsx 	modified:   src/components/server/server-channel.tsx 	modified:   src/components/server/server-member.tsx 	new file:   src/components/ui/sheet.tsx 	modified:   src/components/user-avatar.tsx 	modified:   src/hooks/use-modal-store.ts 	new file:   src/lib/conversation.ts

--- src/app/(main)/(routes)/servers/[serverId]/page.tsx
+++ src/app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,7 +1,46 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { RedirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const ServerPage = () => {
-  return <div>ID</div>;
+interface ServerPageProps {
+  params: {
+    serverId: string;
+  };
+}
+
+const ServerPage = async ({ params }: ServerPageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return <RedirectToSignIn />;
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: { some: { profileId: profile.id } },
+    },
+    include: {
+      channels: {
+        where: {
+          name: "general",
+        },
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+    },
+  });
+
+  const initalChannel = server?.channels[0];
+
+  if (initalChannel?.name !== "general") {
+    return null;
+  }
+
+  return redirect(`/servers/${params.serverId}/channels/${initalChannel?.id}`);
 };
 
 export default ServerPage;
--- src/components/server/server-channel.tsx
+++ src/components/server/server-channel.tsx
@@ -5,6 +5,7 @@ import { Channel, ChannelType, MemberRole, Server } from "@prisma/client";
 import { Edit, Hash, Lock, Mic, Trash, Video } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import { ActionTooltip } from "../action-tooltip";
+import { ModalType, useModal } from "@/hooks/use-modal-store";
 
 interface ServerChannelProps {
   channel: Channel;
@@ -22,11 +23,22 @@ const ServerChannel = ({ channel, server, role }: ServerChannelProps) => {
   const params = useParams();
   const router = useRouter();
 
+  const { onOpen } = useModal();
+
+  const onClick = () => {
+    router.push(`/servers/${params?.serverId}/channels/${channel.id}`);
+  };
+
+  const onAction = (e: React.MouseEvent, action: ModalType) => {
+    e.stopPropagation();
+    onOpen(action, { channel, server });
+  };
+
   const Icon = iconMap[channel.type];
 
   return (
     <button
-      onClick={() => {}}
+      onClick={onClick}
       className={cn(
         "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
         params?.channelId === channel.id && "bg-zinc-700/20 dark:bg-zinc-700"
@@ -45,10 +57,16 @@ const ServerChannel = ({ channel, server, role }: ServerChannelProps) => {
       {channel.name !== "general" && role !== MemberRole.GUEST && (
         <div className="ml-auto flex items-center gap-x-2">
           <ActionTooltip label="Edit">
-            <Edit className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+            <Edit
+              onClick={(e) => onAction(e, "editChannel")}
+              className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+            />
           </ActionTooltip>
-          <ActionTooltip label="Edit">
-            <Trash className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+          <ActionTooltip label="Delete">
+            <Trash
+              onClick={(e) => onAction(e, "deleteChannel")}
+              className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+            />
           </ActionTooltip>
         </div>
       )}
